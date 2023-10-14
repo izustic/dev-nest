b20d672f1e12aba767ff9dd8826d38f7c38ff0c4
izuchukwu obi <izuchukwuobi997@gmail.com>
2023-10-14 16:31:29 +0100

    fixed render error

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,14 +14,14 @@ import { icons, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const  Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState("Full-time");
 
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Izu </Text>
+        <Text style={styles.userName}>Hello Izu</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
